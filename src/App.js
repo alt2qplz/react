d8402d4b359d361e0beee971248f7f0176e89b1f
Alexander Savelichev <alt2qplz@gmail.com>
2020-03-30 14:57:35 +0300

    test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import './App.css';
 import Header from "./Header";
 import Tech from "./Tech";
+import Footer from "./Footer";
+
+
+// Это просто комментарий, что тут такого?
+
 
 const App = () => {
-  return (
-      <div>
-          <Header />
-          <Tech />
-      </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Tech />
+            <Footer/>
+        </div>
+    );
 };
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <ul>
+            <li><a>Главная</a></li>
+            <li><a>Мы</a></li>
+            <li><a>Курсы</a></li>
+        </ul>
+    );
+};
+
+export default Footer;
