47e471a816f06784462d6878449a047b4622c573
Alexander Savelichev <alt2qplz@gmail.com>
2020-03-30 14:26:41 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+import Tech from "./Tech";
+
+const App = () => {
+  return (
+      <div>
+          <Header />
+          <Tech />
+      </div>
+  );
+};
+
+export default App;
---
+++ src/Tech.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Tech = () => {
+    return (
+        <div className="App">
+            <ul>
+                <li>123</li>
+                <li>321</li>
+                <li>23</li>
+            </ul>
+        </div>
+    )
+};
+
+export default Tech;
