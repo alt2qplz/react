299b08ad10b8786d19db75eb378a6fac3f3da4d9
Alexander Savelichev <alt2qplz@gmail.com>
2020-04-01 15:14:45 +0300

    урок 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRAZWYIPyXuDqI99-_2zURPvzu4Hq9kja7D-QxcBSh508lcXSOF" alt=""/>
+        </header>
+    )
+};
+
+export default Header;
